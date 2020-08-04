72a6186d605f1285bb1d435c541c1ecdc8e3aee0
Nazim Leulmi <naz@nazimleulmi.com>
2020-08-04 11:01:21 -0700

    Refactoring to static HTML,CSS,& JS for the simplicity + performance  of the website

--- aster-electric/js/index.js
+++ aster-electric/js/index.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const options = {
     preventScrolling: false
   }
-  const elems = document.querySelectorAll('.sidenav');
-  const instances = M.Sidenav.init(elems, options);
+  const sideNav = document.querySelectorAll('.sidenav');
+  const parallax = document.querySelectorAll('.parallax');
+  const carousel = document.querySelectorAll('.carousel');
+  const scrollspy = document.querySelectorAll('.scrollspy');
+  const parallaxInstance = M.Sidenav.init(sideNav, options);
+  const sideNavInstance = M.Parallax.init(parallax, options);
+  const carouselInstance = M.Carousel.init(carousel, options);
+  const scrollspyInstance = M.ScrollSpy.init(scrollspy, {
+    scrollOffset: 0,
+    throttle: 50,
+    activeClass: "active"
+  });
 });
